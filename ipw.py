f9494565a21e210ec475d2329effc5bd8a79036f
KingBash007 <KingBash2007@gmail.com>
2024-09-18 11:43:07 +0100

    first commit

---
+++ ipw.py
@@ -0,0 +1,11 @@
+# Program to showcase working off different types of bitwise operators
+num1 = 10
+num2 = 4 
+# Using AND operator
+print("num1 & num2 =", num1 & num2)
+# Using OR operator
+print("\n num1 | num2", num1 | num2)
+# Using NOT operator
+print("\n num1 ", ~num1 ) 
+# Using XOR operator
+print("\n num1 ^ num2", num1 ^ num2)
